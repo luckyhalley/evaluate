13e86f88e9f557102d577be4498b32bc2f433657
luckyhalley <mky81@126.com>
2016-10-19 06:30:09 +0800

    Update: lint

--- src/js/container.js
+++ src/js/container.js
@@ -11,7 +11,7 @@ class Container extends Component {
   constructor (props) {
     super(props);
   }
-  componentWillMount() {
+  componentWillMount () {
     let {
       actionGetCourseInfo,
       actionGetScore
